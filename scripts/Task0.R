a3be207b0062fab4a5d12cb9b587d4193eb9f642
DanielVolcak <danielvolcak2004@gmail.com>
2026-02-19 23:56:02 +0200

    Sutvarkytas main failas Task0.R

--- scripts/Task0.R
+++ scripts/Task0.R
@@ -3,9 +3,24 @@
 # analizės paleidimas-sujungimas musu main scriptas
 # to failo tikslas isikleti paruostas duomnis iriskves to to kiekvieno is musu dali per source()
 
-#pvz ikelimas duomenu
-dat<- readRDS("data/dat.rds")
-tm <- readRDS("data/tm.rds") 
+##pvz ikelimas duomenu
+
+#dat<- readRDS("data/dat.rds")
+#tm <- readRDS("data/tm.rds") 
+
 # ir po to per source() kiekvieno dalis pvz
-source("scripts/01_simptomai.R")
+
+#source("scripts/01_simptomai.R")
+#source("scripts/02_top_produktai.R")
+
+
+# scripts/task0.R
+# Pagrindinis analizės failas: sujungia visas komandos dalis
+
+dat <- readRDS("data/dat.rds")
+tm  <- readRDS("data/tm.rds")
+
+# kiekvieno dalys:
+source("scripts/01_symptomai18.R")
 source("scripts/02_top_produktai.R")
+source("scripts/03_pre_vs_school.R")
